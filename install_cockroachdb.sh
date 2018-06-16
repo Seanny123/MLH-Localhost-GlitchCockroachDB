7da4c7a51f1ee0f82bc4751a04efc2a62ef072e9
Sean Aubin <seanaubin@gmail.com>
2018-06-16 16:44:21 -0400

    shell scrips are important

---
+++ install_cockroachdb.sh
@@ -0,0 +1,19 @@
+### These lines implement the basic cockraochdb install
+### https://www.cockroachlabs.com/docs/build-a-nodejs-app-with-cockroachdb.html
+
+# Download and extract the latest binary
+if [ ! -f ./cockroach ]; then
+    wget -N https://binaries.cockroachdb.com/cockroach-latest.linux-amd64.tgz
+    tar xfz cockroach-latest.linux-amd64.tgz
+    mv ./cockroach-latest.linux-amd64/cockroach .
+    rm cockroach-latest.linux-amd64.tgz
+    rm -rf ./cockroach-latest.linux-amd64
+    # Instead of following instrutions and copying cockroach to /usr/bin,
+    # Let's set up an alias. This avoids needing to escalation permissions.
+    alias cockroach="./cockroach"
+
+    # basic check for if the above lines worked out OK
+    cockroach version
+else
+    echo "CockroachDB is already installed"
+fi
